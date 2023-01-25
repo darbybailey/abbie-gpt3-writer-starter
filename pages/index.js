04e85878cf8db2b83fc937d9546549f3ef3eeb04
Darby Bailey <d.b.mcdonough@utah.edu>
2023-01-24 22:56:40 -0700

    added kirl logo

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,42 @@
 import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
-import jsPDF from 'jspdf';
+import kirlLogo from '../assets/kirl-logo-circle.png';
+
+
 
 const Home = () => {
-  const [userInput, setUserInput] = useState('');
-  const [apiOutput, setApiOutput] = useState('')
-  const [isGenerating, setIsGenerating] = useState(false)
+const [userInput, setUserInput] = useState('');
 
-  const callGenerateEndpoint = async () => {
-    setIsGenerating(true);
+const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
   
-    console.log("Calling OpenAI...")
-    const response = await fetch('/api/generate', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ userInput }),
-    });
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
 
-    const data = await response.json();
-    const { output } = data;
-    console.log("OpenAI replied...", output.text)
+  const data = await response.json();
+  const { output } = data;
 
-    setApiOutput(`${output.text}`);
-    setIsGenerating(false);
-  }
+  console.log("OpenAI replied...", output.text)
 
-  const downloadPDF = () => {
-    const doc = new jsPDF();
-    doc.text(apiOutput, 10, 10);
-    doc.save('lesson-plan.pdf');
-  }
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
 
-  const onUserChangedText = (event) => {
-    console.log(event.target.value);
-    setUserInput(event.target.value);
-  };
+
+const onUserChangedText = (event) => {
+  console.log(event.target.value);
+  setUserInput(event.target.value);
+};
 
   return (
     <div className="root">
@@ -51,10 +49,10 @@ const Home = () => {
             <h1>KIRL NETWORKS</ h1>
           </div>
           <div className="header-subtitle">
-            <h2>Welcome to the classroom lesson plan idea generator.  Please enter the HAPPY GRADE and NUMBER OF STUDENTS for which you want lesson plan ideas.</h2>
+            <h2>Welcome to the classroom lesson plan idea generator. Please enter the  GRADE and NUMBER OF STUDENTS for which you want lesson plan ideas.</h2>
           </div>
         </div>
-	</div>
+  </div>
 
 
 <div className="prompt-container">
@@ -96,8 +94,8 @@ const Home = () => {
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <Image src={kirlLogo} alt="kirl logo" />
+            <p>Brought to you by Kirl Networks</p>
           </div>
         </a>
       </div>
