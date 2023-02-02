39c1e54992b391968c71946cf8f4d92330861b16
Darby Bailey <d.b.mcdonough@utah.edu>
2023-02-01 22:31:52 -0700

    added pdfmaker, pdf and css changes
    added pdfmaker, pdf and css changes

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,23 +8,23 @@ const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix = 
 `
-Give me four lesson plans, in 40 minute timeslots, starting at 8 a.m., with 5 minute breaks in between.
+Give me two lesson plans, in 60 minute timeslots.
 
-For two of the timeslots, include group activities of 3-4 studends that also meet with the class in a group share activity as a whole at the end of each 40 minutes.
-For two of the timeslots, make the lesson plan an all classroom activity.
+At the top of the out put, include ABBIE AUTOMATED LESSON PLAN OUTPUT (brought to you by Kirl Networks)
+
+For the first timeslot, include think-pair-share group activities of 2 or more students that also meet with the class in a group share activity as a whole at the end of each 40 minutes.
+For one of the timeslots, make the lesson plan an all classroom activity.
 
 For each timeslot, choose from these topics: language, math, science, history, geography, civics, technology, media literacy, culture, art, music, reading. 
-Never repeat a topic between the four lesson plans.
+Never repeat a topic between the two lesson plans.
 Add a title and a recommended book to each timeslot. Also add a simple topic in all caps for each time slot as a header.
 
 Include materials required for a teacher to administer each timeslot.
 Include a description of the group and classroom activites for each timeslot.
 
-Include a 15 minute "physical activity" break after the first timeslots, and a 50 minute "lunch & play" after the third timeslot.
-
-Include TIMES for each timeslot, including breaks, starting at 8 a.m.
-
 Create this in a bulleted and tabbed outline format appropriate for printing.
+
+At the bottom of the output, include this exact text: "These lesson plans are based on think-pair-share, research backed, learning pedagogy."
 `
 ;
 const generateAction = async (req, res) => {
