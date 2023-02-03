c5b67b6ac8ce809486e0f1141390340d704809a7
Darby Bailey <d.b.mcdonough@utah.edu>
2023-02-02 23:34:42 -0700

    new prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,19 +8,21 @@ const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix = 
 `
-Give me two lesson plans, in 60 minute timeslots.
+Give me one lesson plan that would last 60 minutes.
 
 At the top of the out put, include ABBIE AUTOMATED LESSON PLAN OUTPUT (brought to you by Kirl Networks)
 
-For the first timeslot, include think-pair-share group activities of 2 or more students that also meet with the class in a group share activity as a whole at the end of each 40 minutes.
-For one of the timeslots, make the lesson plan an all classroom activity.
+For the timeslot output, include think-pair-share group activities of 2 or more students that also meet with the teacher or parent
+ and the whole group, in a group share activity as a whole at the end of the hour.
+
 
 For each timeslot, choose from these topics: language, math, science, history, geography, civics, technology, media literacy, culture, art, music, reading. 
-Never repeat a topic between the two lesson plans.
-Add a title and a recommended book to each timeslot. Also add a simple topic in all caps for each time slot as a header.
 
-Include materials required for a teacher to administer each timeslot.
+Add a title and an age appropriate book to the lesson plan. Also add a simple topic in all caps as a header.
+
+Include materials required for a teacher or parent to administer the lesson plan.
 Include a description of the group and classroom activites for each timeslot.
+Include a simple narrative that explains why the book is appropriate for the age group, and three top level pedagogical supports it is related to.
 
 Create this in a bulleted and tabbed outline format appropriate for printing.
 
